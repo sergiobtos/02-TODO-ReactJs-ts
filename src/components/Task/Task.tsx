407c8445a42f1a12f780d3143a3935523f43d046
Sergio Silva <sergiobtos@hotmail.com>
2024-01-21 21:29:15 -0800

    BarCodeScanner_01

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -3,6 +3,7 @@ import Trash from '../../assets/Trash';
 import CheckMark from '../../assets/CheckMark';
 import styles from './Task.module.css';
 import { ITask } from '../Todo/Todo';
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 
 export interface TaskProps {
@@ -22,14 +23,21 @@ function Task({ task, onDeleteTask, onCompleted }: TaskProps) {
     onDeleteTask(task)
   }
 
+   const handleUpdate = (err: any, result: any) => {
+    if (result) {
+      console.log(result)
+    }
+  };
+
 
   return (
     <div id="task" className={styles.wrapper}>
-      <div className={`${styles.content} ${!task?.isComplete ? styles.hideCheckMark : styles.showCheckMark} `}>
+     {/*  <div className={`${styles.content} ${!task?.isComplete ? styles.hideCheckMark : styles.showCheckMark} `}>
         <div><span className={styles.circle} onClick={handleIsDone}><CheckMark /></span></div>
         <p>{task?.content}</p>
         <button onClick={handleDeleteTask}><Trash /></button>
-      </div>
+      </div> */}
+     <BarcodeScannerComponent onUpdate={handleUpdate} />
     </div>
   )
 }
